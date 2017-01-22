926f3301b58ce2f3c0455fdabc8b428c0c9904ca
teros0 <best.guess.oni@gmail.com>
2017-01-22 18:50:10 +0200

    final 2nd task

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 
 sudo rm -f /etc/gunicorn.d/hello.py
