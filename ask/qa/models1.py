e330601d916c7929edfcce1dc8b8fdd93f0701d6
teros0 <best.guess.oni@gmail.com>
2017-01-25 16:53:20 +0200

    not proud of it

---
+++ ask/qa/models1.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+    class Meta:
+        ordering = ('-added_at',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('added_at',)
+
+    def __unicode__(self):
+        return 'Answer by {}'.format(self.author)
